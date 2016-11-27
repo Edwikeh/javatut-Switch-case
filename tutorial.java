f99b9d77469040ce7ace9426bdfbd40b4c6e1185
G. Adrian <mc.kingedward@gmail.com>
2016-11-27 13:52:33 +0100

    Create tutorial.java

---
+++ tutorial.java
@@ -0,0 +1,30 @@
+package swhitchcasetut;
+
+import extra.Console;
+
+/**
+ *
+ * @author Edwikeh
+ * 2016. November 27
+ *  I'm using javalib. You can download here: www.edwikeh.net/javalib.jar
+ */
+
+public class SwhitchCaseTUT {
+
+
+    public static void main(String[] args) {
+        
+        int month = Console.readInt("Month : ");
+        
+        switch(month) {
+            case 1 : System.out.println("January"); ; break;
+            case 2 : System.out.println("February"); break;
+            case 3 : System.out.println("March"); break;
+            default : System.out.println("Invalid number");
+            // etc.
+        }
+        
+        
+    }
+    
+}
